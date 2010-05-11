27598748e9da021af4a18f1a3c5db6d4c39cd7d6
cornelius <cornelius@65ff189f-35ee-0310-953c-23049635c3cf>
2010-05-11 12:41:54 +0000

    wefollow:   1. limit tweeters list to 50 items   2. sort tweeters by followers number   3. hide frontpage views if they do not contain any content   4. bugfixes

--- src/zojax/django/harvestrss/urls.py
+++ src/zojax/django/harvestrss/urls.py
@@ -3,6 +3,6 @@ import dselector
 parser = dselector.Parser()
 
 urlpatterns = parser.patterns('zojax.django.harvestrss.views',
-    (r'{id:digits}-{slug:slug}', 'view_article', {}, "view_article")
+    (r'{id:digits}-{slug:chunk}', 'view_article', {}, "view_article")
 )
 
